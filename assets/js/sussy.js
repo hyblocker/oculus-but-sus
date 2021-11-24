6379388440c2a3980a0cd2fec805afb88020319a
Hyblocker <bryan.spiteri@gmail.com>
2021-11-24 10:39:13 +0100

    rgb

--- assets/js/sussy.js
+++ assets/js/sussy.js
@@ -11,6 +11,20 @@ const crewmateDirections = [
 const degToRad = 0.01745329;
 const TIMESTEP = 1.0 / 30.0;
 const crewmates = [];
+const crewmateIds = [];
+const crewmatePalettes = [
+	{ body: "0a4d2e", shadow: "11802d", outline: "052519" }, // green
+	{ body: "a8044c", shadow: "ff1010", outline: "230110" }, // red
+	{ body: "1E1F26", shadow: "3F474E", outline: "000000" }, // gray
+	{ body: "3B177C", shadow: "6B2FBB", outline: "1C003F" }, // purple
+	{ body: "C38822", shadow: "FFEB5A", outline: "432800" }, // yellow
+	{ body: "AC2BAE", shadow: "EE54BB", outline: "691A6B" }, // pink
+	{ body: "5E2615", shadow: "71491E", outline: "38160C" }, // brown
+	{ body: "15A842", shadow: "50F039", outline: "0A511F" }, // lime
+	{ body: "09158E", shadow: "132ED2", outline: "030938" }, // blue
+	{ body: "B43E15", shadow: "F07D0D", outline: "381306" }, // orange
+	{ body: "8495C0", shadow: "D7E1F1", outline: "242935" }, // light gray
+];
 
 function spawnCrewmate() {
 	const amogus = document.createElement('div');
@@ -18,16 +32,20 @@ function spawnCrewmate() {
 
 	const crewmateID = Math.floor(Math.random() * 4);
 
-	const amogusImg = document.createElement('img');
-	amogusImg.src = `/assets/impostors/crewmate${crewmateID + 1}.svg`;
-	amogus.appendChild(amogusImg);
+	// const amogusImg = document.createElement('img');
+	// amogusImg.src = `/assets/impostors/crewmate${crewmateID + 1}.svg`;
+	// amogus.appendChild(amogusImg);
+	amogus.innerHTML = crewmateIds[crewmateID];
+	const amgousImg = amogus.children[0];
 
 	let rotation = Math.random() * 360.0;
 	let posX = Math.random() * document.body.clientWidth;
 	let posY = Math.random() * document.body.scrollHeight;
 	let scale = Math.random() * 0.6 + 0.8;
 
-	amogus.style.transform = `scale(${scale}) translate(${posX}px, ${posY}px) rotate(${rotation}deg)`;
+	// amogus.style.transform = `scale(${scale}) translate(${posX}px, ${posY}px) rotate(${rotation}deg)`;
+	const palette = crewmatePalettes[Math.floor(Math.random() * crewmatePalettes.length)];
+	amogus.style.cssText = `--body: #${palette.body}; --shadow: #${palette.shadow}; --outline: #${palette.outline}; transform: scale(${scale}) translate(${posX}px, ${posY}px) rotate(${rotation}deg)`;
 
 	crewmates.push({
 		element: amogus,
@@ -51,7 +69,12 @@ crewmateDirections.forEach(dir => {
 	dir.y /= len;
 });
 
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", async function(e) {
+
+	for (let i = 0; i < 5; i++) {
+		await fetchCrewmate(i);
+	}
+
 	const amogusContainer = document.createElement('div');
 	amogusContainer.classList.add('amogus', 'crewmate-container');
 
@@ -82,8 +105,9 @@ function animate() {
 		if (outOfBounds(impostor)) {
 
 			const crewmateID = Math.floor(Math.random() * 4);
-			const amogusImg = impostor.element.children[0];
-			amogusImg.src = `/assets/impostors/crewmate${crewmateID + 1}.svg`;
+			//const amogusImg = impostor.element.children[0];
+			//amogusImg.src = `/assets/impostors/crewmate${crewmateID + 1}.svg`;
+			impostor.element.innerHTML = crewmateIds[crewmateID];
 
 			impostor.rotation = Math.random() * 360.0;
 			impostor.scale = (Math.random() * 0.6 + 0.8);
@@ -104,4 +128,13 @@ function outOfBounds(impostor) {
 			impostor.position.y > 60 -impostor.scale * 50 &&
 			impostor.position.y < document.body.scrollHeight + impostor.scale * 50
 			);
+}
+
+async function fetchCrewmate(id) {
+	await fetch( `/assets/impostors/crewmate${id + 1}.svg`)
+    	.then(r => r.text())
+    	.then(text => {
+    	    crewmateIds[id] = text;
+    	})
+    	.catch(console.error.bind(console));
 }
